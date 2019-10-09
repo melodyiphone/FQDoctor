0646d9253d73de06c39caf5e35b3b9ba1c21152e
melody <melodyiphone@163.com>
2019-10-09 19:50:54 +0800

    初始化项目

---
+++ CodeObfuscation/CodeObfuscation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CodeObfuscation
+//
+//  Created by MelodyFu on 2019/10/9.
+//  Copyright © 2019 com.bj-doctor. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
